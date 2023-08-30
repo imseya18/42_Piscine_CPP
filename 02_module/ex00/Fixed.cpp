35ac9f5bd6bc85dd4bd9e747fdc3079a257626fe
imseya18 <imseya68@gmail.com>
2023-08-30 17:31:16 +0200

    +

--- 02_module/ex00/Fixed.cpp
+++ 02_module/ex00/Fixed.cpp
@@ -3,22 +3,32 @@
 Fixed::Fixed(): _value(0)
 {
 	std::cout << "Default constructor called" << std::endl;
+
+	return ;
 }
 
 Fixed::Fixed(Fixed const & src)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
+
+	return ;
 }
 
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
+
+	return ;
 }
 
 Fixed &Fixed::operator=(Fixed const & rhs)
 {
 	std::cout << "Copy assignement operator called" << std::endl;
+
+	if ( this != &rhs)
+		this->_value = rhs.getRawBits();
+	return *this;
 }
 
 int Fixed::getRawBits( void ) const
