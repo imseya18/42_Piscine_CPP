1385d9c342c39b42f2e30fc84474ad1ec931ab35
imseya18 <imseya68@gmail.com>
2023-10-06 02:17:49 +0200

    brain_class_ex01

--- 04_module/ex01/Brain.cpp
+++ 04_module/ex01/Brain.cpp
@@ -2,7 +2,26 @@
 
 Brain::Brain()
 {
+	std::ostringstream oss;
+	std::cout << "Default constructor for Brain has beeen called" << std::endl;
+	for (int i = 0; i < 100; i++)
+	{
+		oss << i;
+		this->_ideas[i] = "ideas" + oss.str();
+		oss.str("");
+	}
+}
 
+Brain::Brain(std::string type)
+{
+	std::ostringstream oss;
+	std::cout << "constructor for Brain has beeen called" << std::endl;
+	for (int i = 0; i < 100; i++)
+	{
+		oss << i;
+		this->_ideas[i] = type + " ideas" + oss.str();
+		oss.str("");
+	}
 }
 
 Brain::Brain(Brain const & src)
@@ -12,6 +31,23 @@ Brain::Brain(Brain const & src)
 
 Brain::~Brain()
 {
+	std::cout << "Default destructor for Brain has beeen called" << std::endl;
+}
+
+void Brain::displaySpecificIdea(int i)
+{
+	if(i >= int(sizeof(this->_ideas) / sizeof(this->_ideas[0])))
+	{
+		std::cout << "index is out of range" << std::endl;
+		return ;
+	}
+	std::cout << this->_ideas[i] << std::endl;
+}
+
+void Brain::displayIdeas()
+{
+	for(int i = 0; i < 100; i++)
+		std::cout << this->_ideas[i] << "\n";
 
 }
 
--- 04_module/ex01/Brain.hpp
+++ 04_module/ex01/Brain.hpp
@@ -5,14 +5,17 @@
 
 #include <string>
 #include <iostream>
+#include <sstream>
 
 class Brain
 {
     public:
         Brain();
-        Brain(Brain const & src);
-        ~Brain();
-        
+		Brain(std::string type);
+		Brain(Brain const &src);
+		~Brain();
+        void displaySpecificIdea(int i);
+		void displayIdeas();
         Brain &     operator=(Brain const & rhs);
     private:
 		std::string _ideas[100];
