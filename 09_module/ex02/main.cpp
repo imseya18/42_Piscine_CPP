e207785b3aac02bebe23a259c9906065d8685049
imseya18 <imseya68@gmail.com>
2023-11-07 14:48:36 +0100

    wer

--- 09_module/ex02/main.cpp
+++ 09_module/ex02/main.cpp
@@ -23,21 +23,21 @@ int main(int argc, char **argv)
 		return 0;
 	}
 	PmergeMe test(argc - 1);
-	test.fill_vec(argv);
-	int element[] = {1,4,12,2,222222222};
-	std::vector< unsigned int> test2(element, element + (sizeof(element) / sizeof(element[0])));
-	std::vector< unsigned int> test3;
-	test3.reserve(15);
-	std::sort(test2.begin(), test2.end());
-	for (std::vector<unsigned int>::iterator it = test2.begin(); it != test2.end(); it++)
-		std::cout << " " << *it;
-	std::cout << std::endl;
-	std::sort(test.getVec().begin(), test.getVec().end());
-	test.displayVector();
-
-	std::merge(test.getVec().begin(), test.getVec().end(), test2.begin(), test2.end(),  std::back_inserter(test3));
-	for (std::vector<unsigned int>::iterator it = test3.begin(); it != test3.end(); it++)
-		std::cout << " " << *it;
+	//test.fill_vec(argv);
+	//int element[] = {1,4,12,2,222222222};
+	//std::vector< unsigned int> test2(element, element + (sizeof(element) / sizeof(element[0])));
+	//std::vector< unsigned int> test3;
+	//test3.reserve(15);
+	//std::sort(test2.begin(), test2.end());
+	//for (std::vector<unsigned int>::iterator it = test2.begin(); it != test2.end(); it++)
+	//	std::cout << " " << *it;
+	//std::cout << std::endl;
+	//std::sort(test.getVec().begin(), test.getVec().end());
+	//test.displayVector();
+//
+	//std::merge(test.getVec().begin(), test.getVec().end(), test2.begin(), test2.end(),  std::back_inserter(test3));
+	//for (std::vector<unsigned int>::iterator it = test3.begin(); it != test3.end(); it++)
+	//	std::cout << " " << *it;
 	//std::cout << "Before: ";
 	//test.displayVector();
 	//std::cout << "After: ";
